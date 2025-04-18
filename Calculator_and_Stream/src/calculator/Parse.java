49f28814198e3e63dd86c80aa5a0a0a7d6be2872
LaszAndrea <79246573+LaszAndrea@users.noreply.github.com>
2025-04-18 15:25:47 +0200

    Add files via upload

---
+++ Calculator_and_Stream/src/calculator/Parse.java
@@ -0,0 +1,20 @@
+package org.example.calculator;
+
+public class Parse {
+
+	double[] parser(String[] input) {
+
+		double[] szamok = new double[2];
+
+		for (int i = 0; i < input.length; i++) {
+			try {
+				double szam = Double.parseDouble(input[i]);
+				szamok[i] = szam;
+
+			} catch (NumberFormatException e) {
+				throw new ParseException(e);
+			}
+		}
+		return szamok;
+	}
+}
---
+++ Calculator_and_Stream/src/calculator/ParseException.java
@@ -0,0 +1,12 @@
+package org.example.calculator;
+
+
+public class ParseException extends RuntimeException {
+
+	ParseException(Throwable ex) {
+
+		super(ex);
+
+	}
+
+}
